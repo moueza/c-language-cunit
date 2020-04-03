ddf86c153f4b938ab6da791e71d3c48702dc5cdb
moueza <mouezapeter@gmail.com>
2020-04-03 08:04:17 +0200

    extension

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-  printf("%s %s","Bonjour","Peter");
+  //https://openclassrooms.com/fr/courses/4452741-decouvrez-les-librairies-python-pour-la-data-science/5560976-familiarisez-vous-avec-lecosysteme-python
+  printf("%s %s\n","Bonjour","Peter");
   return 0;
 }
